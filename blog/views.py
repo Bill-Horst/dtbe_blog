8c0317c52e1c019d90caeff19ab6307486da52ef
Bill Horst <billhorst@Bills-MacBook-Pro.local>
2022-08-31 17:26:29 -0600

    added search support for blog with stemming / weighing / trigram similarity and moved to postgresql db

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
+from re import search
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 from taggit.models import Tag
 
@@ -77,6 +79,22 @@ def post_share(request, post_id):
 	else:
 		form = EmailPostForm()
 	return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+def post_search(request):
+	form = SearchForm()
+	query = None
+	results = []
+	if 'query' in request.GET:
+		form = SearchForm(request.GET)
+		if form.is_valid():
+			query = form.cleaned_data['query']
+			# results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+			search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+			search_query = SearchQuery(query)
+			# results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+			results = Post.published.annotate(similarity=TrigramSimilarity('title', query),).filter(similarity__gte=0.1).order_by('-similarity')
+
+	return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
 class PostListView(ListView):
     queryset = Post.published.all()
